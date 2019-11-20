34a3cb3db4ad9ca3166e71de2ba35c41e55ce1d4
Songbai Yan <colorguitar@hotmail.com>
2019-11-20 15:55:41 +0800

    add fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+class FizzBuzz:
+    def fizzBuzz(self, word):
+        if word % 3 == 0:
+            return 'fizz'
+        elif word % 5 == 0:
+            return 'buzz'
+        return word
+
+
+
+# if __name__ == '__main__':
+#     unittest.main()
---
+++ fizzbuzz_test.py
@@ -0,0 +1,26 @@
+import unittest
+from fizzbuzz import FizzBuzz
+
+
+class TestFizzBuzz(unittest.TestCase):
+    def setUp(self):
+        self.fizzbuzz = FizzBuzz()
+
+    def test_should_return_1_when_fizzbuzz_given_1(self):
+        word = 1
+        result = self.fizzbuzz.fizzBuzz(word)
+        self.assertEqual(result, word)
+
+    def test_should_return_fizz_when_fizzbuzz_given_3(self):
+        word = 3
+        result = self.fizzbuzz.fizzBuzz(word)
+        self.assertEqual(result, "fizz")
+
+    def test_should_return_buzz_when_fizzbuzz_given_5(self):
+        word = 5
+        result = self.fizzbuzz.fizzBuzz(word)
+        self.assertEqual(result, 'buzz')
+
+
+if __name__ == '__main__':
+    unittest.main()
