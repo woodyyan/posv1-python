4ca1c84204765eeefce4cab155ec3e36ce968fa5
Songbai Yan <colorguitar@hotmail.com>
2019-11-22 17:16:10 +0800

    added 4A0B and 0A0B test cases

--- guessnumber/guess_number.py
+++ guessnumber/guess_number.py
@@ -1,3 +1,16 @@
 class GuessNumberGame:
-    def play(self, number):
-        return 'a'
+    def __init__(self, answer=None):
+        self.answer = answer
+
+    def play(self, numbers):
+        player_numbers = list(numbers)
+        answer_numbers = list(self.answer)
+        allCorrectCount = 0
+        numbersCorrectCount = 0
+        for number in player_numbers:
+            if number in answer_numbers:
+                if answer_numbers.index(number) == player_numbers.index(number):
+                    allCorrectCount += 1
+                else:
+                    numbersCorrectCount += 1
+        return '%sA%sB' % (allCorrectCount, numbersCorrectCount)
--- guessnumber/guess_number_test.py
+++ guessnumber/guess_number_test.py
@@ -5,5 +5,12 @@ from guessnumber.guess_number import GuessNumberGame
 class TestGuessNumber(unittest.TestCase):
     def test_should_return_0A0B_given_1234_when_answer_is_5678(self):
         game = GuessNumberGame()
+        game.answer = '5678'
         result = game.play('1234')
         self.assertEqual(result, '0A0B')
+
+    def test_should_return_4A0B_given_1234_when_answer_is_1234(self):
+        game = GuessNumberGame()
+        game.answer = '1234'
+        result = game.play('1234')
+        self.assertEqual(result, '4A0B')
