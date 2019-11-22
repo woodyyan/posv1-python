6dbfada622f8768e8d83f2c6f17e1e9715387915
Songbai Yan <colorguitar@hotmail.com>
2019-11-22 17:20:57 +0800

    added 1A1B test cases.

--- guessnumber/guess_number_test.py
+++ guessnumber/guess_number_test.py
@@ -3,14 +3,25 @@ from guessnumber.guess_number import GuessNumberGame
 
 
 class TestGuessNumber(unittest.TestCase):
+    def setUp(self):
+        self.game = GuessNumberGame()
+
     def test_should_return_0A0B_given_1234_when_answer_is_5678(self):
-        game = GuessNumberGame()
-        game.answer = '5678'
-        result = game.play('1234')
+        self.game.answer = '5678'
+        result = self.game.play('1234')
         self.assertEqual(result, '0A0B')
 
     def test_should_return_4A0B_given_1234_when_answer_is_1234(self):
-        game = GuessNumberGame()
-        game.answer = '1234'
-        result = game.play('1234')
+        self.game.answer = '1234'
+        result = self.game.play('1234')
         self.assertEqual(result, '4A0B')
+
+    def test_should_return_2A2B_given_1234_when_answer_is_1243(self):
+        self.game.answer = '1243'
+        result = self.game.play('1234')
+        self.assertEqual(result, '2A2B')
+
+    def test_should_return_1A1B_given_1234_when_answer_is_1357(self):
+        self.game.answer = '1357'
+        result = self.game.play('1234')
+        self.assertEqual(result, '1A1B')
