8aafc85124fd2ff65febdb7a103b48c9191c897d
Songbai Yan <colorguitar@hotmail.com>
2019-11-25 20:14:36 +0800

    refactor parser for args

--- args/parser.py
+++ args/parser.py
@@ -1,3 +1,6 @@
+FLAG_LENGTH = 1
+
+
 class Args:
     def __init__(self, message=None):
         self.message = message
@@ -7,36 +10,90 @@ class Parser:
     def __init__(self, schema):
         self.schema = schema
 
-    def parse(self, message):
+    def parse(self, command):
         args = Args()
 
-        error_message = self.verify_message(message.strip())
+        if command and len(command) > 255:
+            args.message = 'total length of message should not greater than 255.'
+            return args
+
+        # split
+        pairs = self.split(command)
+
+        # verify
+        error_message = self.verify(pairs)
         if error_message:
             args.message = error_message
 
+        # parse
+
         return args
 
-    def verify_message(self, message):
-        if len(message) > 255:
-            return 'total length of message should not greater than 255.'
-        pairs = list(filter(None, message.split(' -')))
-        all_items = message.split(' ')
-        all_flags = list(filter(lambda p: p.startswith('-'), all_items))
-        all_values = list(filter(lambda p: not (p.startswith('-')), all_items))
-        invalid_length_flags = list(filter(lambda f: len(f) != 2, all_flags))
-        if len(invalid_length_flags) > 0:
-            return 'invalid flag length.'
-        elif len(all_flags) != len(set(all_flags)):
-            return 'flags cannot be duplicated.'
-        elif self.has_space_in_value(pairs):
-            return 'invalid value.'
-        elif self.check_unsupported_flags(all_flags):
-            return 'unsupported flag.'
-        return None
-
-    def has_space_in_value(self, pairs):
-        return len(list(filter(lambda p: len(list(filter(None, p.split(' ')))) > 2, pairs))) > 0
+    def has_space_in_value(self, values):
+        return len(list(filter(lambda p: ' ' in p, filter(None, values)))) > 0
 
     def check_unsupported_flags(self, all_flags):
         supported_flags = list(map(lambda key: key, self.schema))
         return len(list(filter(lambda f: f not in supported_flags, all_flags))) > 0
+
+    def check_wrong_type_value(self, pairs):
+        for pair in pairs:
+            if pair.value:
+                value_type = self.schema[pair.flag]
+                if value_type == 'int':
+                    if not pair.value.isdigit():
+                        return 'flag %s type should be %s.' % (pair.flag, value_type)
+                elif value_type == 'str':
+                    if type(pair.value) is not str:
+                        return 'flag %s type should be %s.' % (pair.flag, value_type)
+                elif value_type == 'bool':
+                    if not (pair.value == 'True' or pair.value == 'False'):
+                        return 'flag %s type should be %s.' % (pair.flag, value_type)
+        return None
+
+    def split(self, message):
+        pairs = []
+        if message:
+            items = list(filter(None, message.strip().split('-')))
+            for item in items:
+                flag_value = list(filter(None, item.strip().split(' ')))
+
+                if len(flag_value) > 0:
+                    flag = flag_value[0]
+                    if item.startswith(' '):
+                        flag = ' ' + flag
+                    if len(flag_value) > 1:
+                        flag_value.pop(0)
+                        value = ' '.join(flag_value)
+                        pairs.append(Pair(flag, value))
+                    else:
+                        pairs.append(Pair(flag, None))
+        return pairs
+
+    def verify(self, pairs):
+        all_flags = list(map(lambda p: p.flag, pairs))
+        all_values = list(map(lambda p: p.value, pairs))
+        if self.check_invalid_length_flag(all_flags):
+            return 'invalid flag length.'
+        elif self.check_duplicated_flags(all_flags):
+            return 'flags cannot be duplicated.'
+        elif self.has_space_in_value(all_values):
+            return 'invalid value.'
+        elif self.check_unsupported_flags(all_flags):
+            return 'unsupported flag.'
+        wrong_type_message = self.check_wrong_type_value(pairs)
+        if wrong_type_message:
+            return wrong_type_message
+        return None
+
+    def check_invalid_length_flag(self, all_flags):
+        return len(list(filter(lambda f: len(f) != FLAG_LENGTH, all_flags))) > 0
+
+    def check_duplicated_flags(self, all_flags):
+        return len(all_flags) != len(set(all_flags))
+
+
+class Pair:
+    def __init__(self, flag, value):
+        self.flag = flag
+        self.value = value
--- args/parser_test.py
+++ args/parser_test.py
@@ -18,24 +18,25 @@ class TestParser(unittest.TestCase):
     # test_should_return_unsupported_flags_given_flag_is_l_when_schema_is_f
     # test_should_return_flag_l_type_is_int_given_l_abc_when_schema_is_l_with_int_value
     # test_should_return_flag_d_type_is_string_given_d_123_when_schema_is_d_with_string_value
+    # test_should_return__given_d_123_l_abc_when_schema_is_d_with_int_and_l_with_string_value
     #
     # 默认值
     # test_should_return_default_value_false_0_empty_given_p_d_when_schema_is_i_bool_p_int_d_string
     # test_should_return_default_value_false_0_empty_given_none_message_when_schema_is_i_bool_p_int_d_string
     # test_should_return_true_8080_logs_value_given_i_p_8080_d_logs_when_schema_is_i_bool_p_int_d_string
     def test_should_return_flag_length_should_be_1_given_flag_length_is_2_when_verify_args(self):
-        schema = {'-l': 'int'}
+        schema = {'l': 'int'}
         parser = Parser(schema)
         message = '-ll'
         args = parser.parse(message)
         self.assertEqual(args.message, 'invalid flag length.')
 
-    def test_should_return_flags_length_should_be_1_given_two_flags_without_space(self):
-        schema = {'-l': 'int'}
+    def test_should_return_unsupported_flag_given_two_flags_without_space(self):
+        schema = {'l': 'int'}
         parser = Parser(schema)
         message = '-l-p'
         args = parser.parse(message)
-        self.assertEqual(args.message, 'invalid flag length.')
+        self.assertEqual(args.message, 'unsupported flag.')
 
     def test_should_return_flags_cannot_be_duplicated_given_two_same_flags(self):
         schema = {'-l': 'int'}
@@ -45,28 +46,28 @@ class TestParser(unittest.TestCase):
         self.assertEqual(args.message, 'flags cannot be duplicated.')
 
     def test_should_return_invalid_flag_given_flag_has_space_between_dash_and_letter(self):
-        schema = {'-l': 'int'}
+        schema = {'l': 'int'}
         parser = Parser(schema)
         message = '- l'
         args = parser.parse(message)
         self.assertEqual(args.message, 'invalid flag length.')
 
     def test_should_return_invalid_value_given_value_has_space(self):
-        schema = ''
+        schema = {'l': 'str'}
         parser = Parser(schema)
         message = '-l a b'
         args = parser.parse(message)
         self.assertEqual(args.message, 'invalid value.')
 
     def test_should_return_args_given_multi_space_at_start_end_middle(self):
-        schema = {'-l': 'int'}
+        schema = {'l': 'int'}
         parser = Parser(schema)
         message = '   -l    0   '
         args = parser.parse(message)
         self.assertEqual(args.message, None)
 
     def test_should_return_args_given_multi_space_between_two_flags_when_no_value(self):
-        schema = {'-l': 'int'}
+        schema = {'l': 'int'}
         parser = Parser(schema)
         message = '-l    0'
         args = parser.parse(message)
@@ -87,3 +88,17 @@ class TestParser(unittest.TestCase):
         message = '-l abc'
         args = parser.parse(message)
         self.assertEqual(args.message, 'unsupported flag.')
+
+    def test_should_return_flag_l_type_is_int_given_l_abc_when_schema_is_l_with_int_value(self):
+        schema = {'l': 'int'}
+        parser = Parser(schema)
+        message = '-l abc'
+        args = parser.parse(message)
+        self.assertEqual(args.message, 'flag l type should be int.')
+
+    def test_should_return_correct_args_given_d_123_l_abc_i_when_schema_is_d_int_l_string_i_bool_value(self):
+        schema = {'d': 'int', 'l': 'str', 'i': 'bool'}
+        parser = Parser(schema)
+        message = '  -d   123  -l  abc   -i   '
+        args = parser.parse(message)
+        self.assertEqual(args.message, None)
