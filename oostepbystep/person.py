b176a93347b7729d965471871926647afb966f37
Songbai Yan <colorguitar@hotmail.com>
2019-11-26 11:47:04 +0800

    added ac3 test case

--- oostepbystep/person.py
+++ oostepbystep/person.py
@@ -1,18 +1,21 @@
 class Person:
-    def __init__(self, name, age, id):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.id = id
 
     def introduce(self):
         return 'My name is %s. I am %s years old.' % (self.name, self.age)
 
 
 class Student(Person):
+    def __init__(self, name, age, class_number):
+        super().__init__(name, age)
+        self.class_number = class_number
+
     def introduce(self):
-        return 'My name is %s. I am %s years old. Coding for the glory of HW.' % (self.name, self.age)
+        return 'My name is %s. I am %s years old. I am a student of class %s. Coding for the glory of HW.' % (self.name, self.age, self.class_number)
 
 
-class Coach(Person):
+class Teacher(Person):
     def introduce(self):
         return 'My name is %s. I am %s years old. Teaching for the future of world.' % (self.name, self.age)
--- oostepbystep/person_test.py
+++ oostepbystep/person_test.py
@@ -1,26 +1,26 @@
 import unittest
 
-from oostepbystep.person import Person, Student, Coach
+from oostepbystep.person import Person, Student, Teacher
 
 
 class TestPerson(unittest.TestCase):
     def test_should_person_have_name_and_age(self):
-        person = Person('Tom', '21', 1)
-        self.assertEqual(person.id, 1)
+        person = Person('Tom', '21')
         self.assertEqual(person.name, 'Tom')
         self.assertEqual(person.age, '21')
 
     def test_should_person_have_an_introduce_method_which_introduce_person_with_name_and_age(self):
-        tom = Person('Tom', '21', 2)
+        tom = Person('Tom', '21')
         introduce = tom.introduce()
         self.assertEqual(introduce, 'My name is Tom. I am 21 years old.')
 
-    def test_should_student_introduce(self):
-        tom = Student('Tom', '18', 1)
-        introduce = tom.introduce()
-        self.assertEqual(introduce, 'My name is Tom. I am 18 years old. Coding for the glory of HW.')
-
     def test_should_coach_introduce(self):
-        tom = Coach('Tom', '20', 1)
+        tom = Teacher('Tom', '20')
         introduce = tom.introduce()
         self.assertEqual(introduce, 'My name is Tom. I am 20 years old. Teaching for the future of world.')
+
+    def test_should_student_introduce_with_class(self):
+        tom = Student('Tom', '20', 2)
+        introduce = tom.introduce()
+        self.assertEqual(introduce,
+                         'My name is Tom. I am 20 years old. I am a student of class 2. Coding for the glory of HW.')
