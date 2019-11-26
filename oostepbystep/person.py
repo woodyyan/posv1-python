5846ee02cf7ccef2257534d72768726dd62911a1
Songbai Yan <colorguitar@hotmail.com>
2019-11-26 11:58:14 +0800

    added Klass

--- oostepbystep/person.py
+++ oostepbystep/person.py
@@ -17,5 +17,20 @@ class Student(Person):
 
 
 class Teacher(Person):
+    def __init__(self, name, age, class_numbers):
+        super().__init__(name, age)
+        self.class_numbers = class_numbers
+
     def introduce(self):
         return 'My name is %s. I am %s years old. Teaching for the future of world.' % (self.name, self.age)
+
+
+class Klass:
+    def add_student(self, student):
+        pass
+
+    def set_leader(self, student):
+        pass
+
+    def set_teacher(self, teacher):
+        pass
