98c0a8be9634bbe67ce055724d4156e64b0bd7f8
Songbai Yan <colorguitar@hotmail.com>
2019-12-01 19:18:41 +0800

    refactor guess number and added mock

--- guessnumber/guess_number.py
+++ guessnumber/guess_number.py
@@ -1,6 +1,7 @@
 import random
 
-CORRECT_ANSWER = '4A0B'
+ANSWER_FORMAT = '%sA%sB'
+CORRECT_ANSWER_FORMAT = '4A0B'
 
 
 def get_player_answer():
@@ -11,13 +12,14 @@ def get_player_answer():
 
 class GuessNumberGame:
     ROUND_COUNT = 6
+    __answer = ''
 
-    def __init__(self, answer=None):
-        self.answer = answer
+    def __init__(self, answer_generator):
+        self.__answer = answer_generator.generate()
 
-    def play(self, numbers):
+    def guess(self, numbers):
         player_numbers = list(numbers)
-        answer_numbers = list(self.answer)
+        answer_numbers = list(self.__answer)
         allCorrectCount = 0
         numbersCorrectCount = 0
         for number in player_numbers:
@@ -26,19 +28,18 @@ class GuessNumberGame:
                     allCorrectCount += 1
                 else:
                     numbersCorrectCount += 1
-        return '%sA%sB' % (allCorrectCount, numbersCorrectCount)
+        return ANSWER_FORMAT % (allCorrectCount, numbersCorrectCount)
 
-    def run(self):
+    def play(self):
         isSuccess = False
         for index in range(GuessNumberGame.ROUND_COUNT):
-            player_answer = get_player_answer()
-            answer = self.play(player_answer)
-            if answer == CORRECT_ANSWER:
+            answer = self.guess(get_player_answer())
+            if answer == CORRECT_ANSWER_FORMAT:
                 isSuccess = True
                 break
             else:
                 print(answer)
-        print('Game Over! You Win!' if isSuccess else 'Game Over! You Lose! Answer is %s' % self.answer)
+        print('Game Over! You Win!' if isSuccess else 'Game Over! You Lose! Answer is %s' % self.__answer)
 
 
 class AnswerGenerator:
@@ -50,6 +51,5 @@ class AnswerGenerator:
 
 
 if __name__ == '__main__':
-    game = GuessNumberGame()
-    game.answer = AnswerGenerator().generate()
-    game.run()
+    game = GuessNumberGame(AnswerGenerator())
+    game.play()
--- guessnumber/guess_number_test.py
+++ guessnumber/guess_number_test.py
@@ -1,39 +1,34 @@
 import unittest
+from unittest import mock
 from guessnumber.guess_number import GuessNumberGame, AnswerGenerator
 
 
 class TestGuessNumber(unittest.TestCase):
     def setUp(self):
-        self.game = GuessNumberGame()
+        self.game = GuessNumberGame(StubAnswerGenerator())
 
-    def test_should_return_0A0B_given_1234_when_answer_is_5678(self):
-        self.game.answer = '5678'
-        result = self.game.play('1234')
+    def test_should_return_0A0B_given_5678_when_answer_is_1234(self):
+        result = self.game.guess('5678')
         self.assertEqual(result, '0A0B')
 
     def test_should_return_4A0B_given_1234_when_answer_is_1234(self):
-        self.game.answer = '1234'
-        result = self.game.play('1234')
+        result = self.game.guess('1234')
         self.assertEqual(result, '4A0B')
 
-    def test_should_return_2A2B_given_1234_when_answer_is_1243(self):
-        self.game.answer = '1243'
-        result = self.game.play('1234')
+    def test_should_return_2A2B_given_1243_when_answer_is_1234(self):
+        result = self.game.guess('1243')
         self.assertEqual(result, '2A2B')
 
-    def test_should_return_1A1B_given_1234_when_answer_is_1357(self):
-        self.game.answer = '1357'
-        result = self.game.play('1234')
+    def test_should_return_1A1B_given_1357_when_answer_is_1234(self):
+        result = self.game.guess('1357')
         self.assertEqual(result, '1A1B')
 
-    def test_should_return_0A4B_given_1234_when_answer_is_4321(self):
-        self.game.answer = '4321'
-        result = self.game.play('1234')
+    def test_should_return_0A4B_given_4321_when_answer_is_1234(self):
+        result = self.game.guess('4321')
         self.assertEqual(result, '0A4B')
 
     def test_should_return_0A1B_given_1234_when_answer_is_5671(self):
-        self.game.answer = '1234'
-        result = self.game.play('5671')
+        result = self.game.guess('5671')
         self.assertEqual(result, '0A1B')
 
     def test_should_return_4_digits_when_generate_answer(self):
@@ -51,9 +46,12 @@ class TestGuessNumber(unittest.TestCase):
         answer = answerGenerator.generate()
         self.assertEqual(len(set(answer)), 4)
 
-    def test_should(self):
-        self.game.answer = '1234'
-        self.game.run()
+    def test_should_return_4A0B_given_1234_when_answer_is_1234_with_mock(self):
+        generator = AnswerGenerator()
+        generator.generate = mock.Mock(return_value='1234')
+        game = GuessNumberGame(generator)
+        result = game.guess('1234')
+        self.assertEqual(result, '4A0B')
 
 
 class StubAnswerGenerator(AnswerGenerator):
